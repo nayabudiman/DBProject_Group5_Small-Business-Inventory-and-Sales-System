a89a3eea6cf34058e6bf3f1c44dbd9e26c84cf66
Pison Golda <pison.gm.dev@gmail.com>
2025-11-25 10:16:33 +0700

    modify the hook to redirect root route to auth and dashboard

--- week4_integration/retail-management-web/src/hooks.server.ts
+++ week4_integration/retail-management-web/src/hooks.server.ts
@@ -4,8 +4,15 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
     const sessionToken = event.cookies.get('session_token');
 
+    if (event.url.pathname === '/') {
+        if (sessionToken) {
+            throw redirect(303, '/dashboard/users');
+        } else {
+            throw redirect(303, '/auth');
+        }
+    }
+
     if (event.url.pathname.startsWith('/dashboard')) {
-        
         if (!sessionToken) {
             throw redirect(303, '/auth');
         }
